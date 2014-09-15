47e9aed46b232ff742fab37987b7ede8b2cd2964
Jeremy Morgan <jeremymorgan@gmail.com>
2014-09-15 11:17:27 -0700

    Initial Commit

---
+++ WhatsMyIP/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Windows;
+
+namespace WhatsMyIP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LabelIP.Content = GetMyIp();
+        }
+
+        public string GetMyIp()
+        {
+            string localIp = "?";
+            
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIp = ip.ToString();
+                }
+            }
+            return localIp;
+        }
+    }
+}
